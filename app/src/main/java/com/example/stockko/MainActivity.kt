87b725b31d133cd2f1e4c5cb7be6a9b4189b71c1
aysegullsahin <aysegul.sahin004@gmail.com>
2019-12-12 21:33:02 +0300

    commit

--- app/src/main/java/com/example/stockko/MainActivity.kt
+++ app/src/main/java/com/example/stockko/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.example.stockko
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.stockko.ui.main.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
+        barkodbtn.setOnClickListener{
+            var inflater = LayoutInflater.from(this)
+            var newProduct = inflater.inflate(R.layout.new_activity_main,null)
+            rootLayout.addView(newProduct)
+
+        }
+
     }
 }
+
+
