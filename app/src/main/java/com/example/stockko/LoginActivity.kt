fba6d82349baa275ad347e5c139ea5698edc6863
aysegullsahin <aysegul.sahin004@gmail.com>
2020-01-13 18:11:03 +0300

    register page and login page

---
+++ app/src/main/java/com/example/stockko/LoginActivity.kt
@@ -0,0 +1,54 @@
+package com.example.stockko
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        tvKayitOl.setOnClickListener {
+
+            var intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnGirisYap.setOnClickListener {
+
+            if (etSifre.text.isNotEmpty() && etSifre.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(etMail.text.toString(),etSifre.text.toString())
+                    .addOnCompleteListener(object:OnCompleteListener<AuthResult>{
+                        override fun onComplete(p0: Task<AuthResult>) {
+
+                            if(p0.isSuccessful){
+                                Toast.makeText(this@LoginActivity, "Başarılı Giriş."+FirebaseAuth.getInstance().currentUser?.email,Toast.LENGTH_SHORT).show()
+                                var intent1 = Intent(this@LoginActivity,MainActivity::class.java)
+                                startActivity(intent1)
+                            }
+
+                            else{
+                                Toast.makeText(this@LoginActivity, "Hatalı Giriş"+ p0.exception?.message,Toast.LENGTH_SHORT).show()
+
+                            }
+                        }
+
+                    })
+
+            }
+            else{
+                Toast.makeText(this@LoginActivity, "Boş alanları doldurunuz",Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/stockko/RegisterActivity.kt
+++ app/src/main/java/com/example/stockko/RegisterActivity.kt
@@ -1,12 +1,59 @@
 package com.example.stockko
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        btnKaydol.setOnClickListener {
+            if(etMail.text.isNotEmpty() && etSifre.text.isNotEmpty() && etSifreTekrar.text.isNotEmpty() && etAd.text.isNotEmpty() && etMagazaAdi.text.isNotEmpty() && etMagazaAdres.text.isNotEmpty()) {
+                if(etSifre.text.toString().equals(etSifreTekrar.text.toString())){
+
+                    yeniUyeKayit(etMail.text.toString(),etSifre.text.toString())
+                    var intent2 = Intent(this@RegisterActivity,LoginActivity::class.java)
+                    startActivity(intent2)
+                }
+                else{
+                    Toast.makeText(this,"Şifreler aynı değil",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(this,"Boş alanları doldurunuz",Toast.LENGTH_SHORT).show()
+            }
+            }
+        }
+
+    private fun yeniUyeKayit(mail: String, sifre: String) {
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(mail,sifre)
+            .addOnCompleteListener(object:OnCompleteListener<AuthResult>{
+                override fun onComplete(p0: Task<AuthResult>) {
+
+                    if (p0.isSuccessful) {
+
+                        Toast.makeText(this@RegisterActivity, "Üye kaydedildi"+FirebaseAuth.getInstance().currentUser?.uid, Toast.LENGTH_SHORT).show()
+                        FirebaseAuth.getInstance().signOut()
+
+                    }
+
+                    else{
+                        Toast.makeText(this@RegisterActivity,"Üye kaydedilirken sorun çıktı"+p0.exception?.message,Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            })
+
     }
+
 }
+
